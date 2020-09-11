c19eee298d477f64005aa0ad6287d53c3783146f
Sigi Jaeckel <sigij55@gmail.com>
2020-09-11 14:27:22 -0400

    fixed ring_buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,50 +5,37 @@ class RingBuffer:
         self.list = []
         self.tail = 0
         self.head = 0
-        self.buffer = [None] * self.capacity
+        # self.buffer = [None] * self.capacity
         self.cursor = 0
 
     def append(self, item):
-        if self.size == self.capacity:
+        if len(self.list) == self.capacity:
             self.list[self.cursor] = item
-            self.cursor += 1
-            if self.cursor == self.capacity:
-                self.cursor = 0
-            if self.size <= self.capacity:
-                self.size += 1
+            self.cursor = (self.cursor+1) % self.capacity
         else:
             self.list.append(item)
-            self.size += 1
-        # if self.size 
-
-
-        # if self.size >= self.capacity:
-        #     self.list[self.head] = item
-        #     if self.head == self.capacity - 1:
-        #         self.head = 0
-        #     else:
-        #         self.head += 1
-        #     if self.tail == self.capacity - 1:
-        #         self.tail = 0
-        #     else:
-        #         self.tail += 1
-        #     self.size += 1
-        # else:
-        #     self.list.append(item)
-        #     self.tail = self.tail + 1
-        #     self.size += 1
             
-
-
-
     def get(self):
-        if self.size == 0:
-            return
-       
-        else:
-            for item in self.list:
-                if item is not None:
-                    print(item)
+        l = []
+        for item in self.list:
+            if item is not None:
+                l.append(item)
+        return l
+
+# test_buffer = RingBuffer(5)
+
+# test_buffer.append(1)
+# test_buffer.append(2)
+# test_buffer.append(3)
+# test_buffer.append(4)
+# test_buffer.append(5)
+# test_buffer.append(6)
+# test_buffer.append(7)
+# test_buffer.append(8)
+
+# test_buffer.get()
+
+
 
                 
 
